088f8b8d8fdde7c0d99fd21aa023f3c35096d8ba
wmzy <1256573276@qq.com>
2022-10-28 08:59:35 +0800

    fix: type error

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import {readFile} from 'fs/promises';
-import type {Plugin} from 'vite';
+import { readFile } from 'fs/promises';
+import type { Plugin } from 'vite';
 
 export type Options = {
   name?: string;
-  enforce?: Plugin['enforce'],
+  enforce?: Plugin['enforce'];
   virtualPrefix?: false | string;
   map(file: string): string | void | undefined;
-}
+};
 
-export default function resolveAsPlugin({ map, virtualPrefix = '\0', ...options }: Options): Plugin {
+export default function resolveAsPlugin({
+  map,
+  virtualPrefix = '\0',
+  ...options
+}: Options): Plugin {
   const virtualToRealMap = new Map<string, string>();
   const realToVirtualMap = new Map<string, string>();
 
@@ -18,18 +22,18 @@ export default function resolveAsPlugin({ map, virtualPrefix = '\0', ...options 
     ...options,
     resolveId(id, importer, options) {
       if (virtualToRealMap.has(id)) return id;
-      const resolve = (i) => this.resolve(i, importer, { ...options, skipSelf: true });
-      return resolve(id)
-        .then((resolved) => {
+      return this.resolve(id, importer, { ...options, skipSelf: true }).then(
+        resolved => {
           if (!resolved || resolved.external) return resolved;
           const newFile = map(resolved.id);
           if (!newFile || newFile === resolved.id) return resolved;
           const id = virtualPrefix ? virtualPrefix + newFile : newFile;
 
-            virtualToRealMap.set(id, resolved.id);
-            realToVirtualMap.set(resolved.id, id);
-            return { ...resolved, id };
-        });
+          virtualToRealMap.set(id, resolved.id);
+          realToVirtualMap.set(resolved.id, id);
+          return { ...resolved, id };
+        }
+      );
     },
     load(id, options) {
       const realFile = virtualToRealMap.get(id);
@@ -38,7 +42,7 @@ export default function resolveAsPlugin({ map, virtualPrefix = '\0', ...options 
       }
     },
     configureServer(server) {
-      server.watcher.on('change', (file) => {
+      server.watcher.on('change', file => {
         const virtualFile = realToVirtualMap.get(file);
         if (virtualFile) {
           server.watcher.emit('change', virtualFile);
